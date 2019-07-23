425111e8f643ef0be7d6a2cb4f12e382855c8ad9
Patrick Rigney <patrick@toggledbits.com>
2019-07-23 16:53:21 -0400

    jshint fixup

--- J_IntesisWMPGateway1_ALTUI.js
+++ J_IntesisWMPGateway1_ALTUI.js
@@ -1,16 +1,18 @@
 //# sourceURL=J_IntesisWMPGateway1_ALTUI.js
+/* globals window,MultiBox */
+
 "use strict";
 
 var IntesisWMPGateway_ALTUI = ( function( window, undefined ) {
 
-        function _draw( device ) {
-                var html ="";
-                var s;
-                s += MultiBox.getStatus( device, "urn:toggledbits-com:serviceId:IntesisWMPGateway1", "DisplayStatus");
-                html += '<div>' + s + '</div>';
-                return html;
-        }
-    return {
-        DeviceDraw: _draw,
-    };
+		function _draw( device ) {
+				var html ="";
+				var s;
+				s += MultiBox.getStatus( device, "urn:toggledbits-com:serviceId:IntesisWMPGateway1", "DisplayStatus");
+				html += '<div>' + s + '</div>';
+				return html;
+		}
+	return {
+		DeviceDraw: _draw,
+	};
 })( window );
